0c83f2b03e98016498132723475ded160edd4d54
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-16 14:01:19 -0500

    create the dropdown with js and add the if statement

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -5,7 +5,36 @@ import {mountainsArray} from "./scripts-data/mountainData.js";
 window.onload = () =>{
     // creating a dropdown of the mountains name 
 
-    mountainsArray.map(() =>{
+    mountainsArray.map((mounNames) =>{
+        console.log(mounNames.name)
+        let namesOption = new Option(mounNames.name);
+        let optionEle = document.getElementById('mountainNames');
+        optionEle.appendChild(namesOption);
+        optionEle.addEventListener('change', () => {
+            if(mounNames.name == optionEle.value ){
+                console.log(`it works`);
+                console.log(mounNames);
+                let divEle = document.getElementById('displayMountInfo');
+
+                 () => {
+                   
+                    divEle.innerHTML += `
+                    <tr>
+                    <h2>${mounNames.name}</h2>
+                    <p>${mounNames.desc}</p>
+                    <img>${mounNames.img}</img>
+
+                    
+                    `;
+                }
+                
+            }
+        });
+      
+
         
-    })
+    
+    });
+    
+
 }
