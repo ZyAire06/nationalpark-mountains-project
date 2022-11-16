1e048ad3fedffa4fe56b6f022de6ad9a0e786623
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-16 12:04:46 -0500

    flusing out js file and importing the mountainsData.js

---
+++ scripts/mountains.js
@@ -0,0 +1,11 @@
+// importing the mountainsData.js 
+import {mountainsArray} from "./scripts-data/mountainData.js";
+
+
+window.onload = () =>{
+    // creating a dropdown of the mountains name 
+
+    mountainsArray.map(() =>{
+        
+    })
+}
